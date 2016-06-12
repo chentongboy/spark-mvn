7069c8086f69c11435d951601ae29df31f65e646
kong <konvish@163.com>
2016-06-12 14:43:19 +0800

    信令数据基础分析

---
+++ spark-app/src/main/java/com/kong/spark/rdd/CUAnalysis.java
@@ -0,0 +1,120 @@
+package com.kong.spark.rdd;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import scala.Tuple2;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+
+/**
+ * Created by lenovo on 2016/6/12.
+ */
+public class CUAnalysis {
+    public static void main(String[] args) {
+        final SparkConf conf = new SparkConf().setAppName("CUAnalysis");
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+        JavaRDD<String> map = jsc.textFile("C:\\kong\\联通信令数据\\信息样本数据（20151019——20151025）.csv").map(new Function<String, String>() {
+            public String call(String s) throws Exception {
+                String[] splited = s.split(",");
+                String startTime = splited[2];
+                if (startTime.length() < 6) {
+                    StringBuffer sb = null;
+                    for (int i = 0; i < 6 - startTime.length(); i++) {
+                        sb.append("0");
+                    }
+                    startTime = sb.toString() + startTime;
+                }
+                String longitude = splited[3];
+                String latitude = splited[4];
+                String areaCode = splited[5];
+                String userId = splited[0] + "," + splited[1] + "," + startTime;
+                if (userId.length() <= 16 || Double.valueOf(longitude) < 113.766667 || Double.valueOf(longitude) > 114.616667 || Double.valueOf(latitude) < 22.450000 || Double.valueOf(latitude) > 22.866667)
+                    return null;
+                return userId + "," + longitude + "," + latitude + "," + areaCode;
+            }
+        }).filter(new Function<String, Boolean>() {
+            public Boolean call(String s) throws Exception {
+                return !s.equals("") && s != null;
+            }
+        });
+
+        //每个用户的记录数
+        JavaPairRDD<String, Integer> user = map.mapToPair(new PairFunction<String, String, Integer>() {
+            public Tuple2<String, Integer> call(String string) throws Exception {
+                String[] split = string.split(",");
+                return new Tuple2<String, Integer>(split[0], 1);
+            }
+        }).reduceByKey(new Function2<Integer, Integer, Integer>() {
+            public Integer call(Integer integer, Integer integer2) throws Exception {
+                return integer + integer2;
+            }
+        });
+
+        //总用户数
+        JavaPairRDD<String, Integer> userCount = map.mapToPair(new PairFunction<String, String, Integer>() {
+            public Tuple2<String, Integer> call(String s) throws Exception {
+                String[] split = s.split(",");
+                return new Tuple2<String, Integer>(split[0], 1);
+            }
+        }).groupByKey().mapToPair(new PairFunction<Tuple2<String, Iterable<Integer>>, String, Integer>() {
+            public Tuple2<String, Integer> call(Tuple2<String, Iterable<Integer>> tuple2) throws Exception {
+                return new Tuple2<String, Integer>(tuple2._1, 1);
+            }
+        }).reduceByKey(new Function2<Integer, Integer, Integer>() {
+            public Integer call(Integer integer, Integer integer2) throws Exception {
+                return integer + integer2;
+            }
+        });
+
+        //总记录数
+        map.count();
+
+        //构建用户时间
+        JavaPairRDD<String, Long> pairRDD = map.mapToPair(new PairFunction<String, Long, String>() {
+            public Tuple2<Long, String> call(String s) throws Exception {
+                String[] split = s.split(",");
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HHmmss");
+                String time = split[1] + "-" + split[2];
+                long timestamp = sdf.parse(time).getTime();
+                return new Tuple2<Long, String>(timestamp, split[0]);
+            }
+        }).sortByKey().mapToPair(new PairFunction<Tuple2<Long, String>, String, Long>() {
+            public Tuple2<String, Long> call(Tuple2<Long, String> longStringTuple2) throws Exception {
+                return new Tuple2<String, Long>(longStringTuple2._2, longStringTuple2._1);
+            }
+        });
+
+        //每个用户的平均时间间隔
+        JavaPairRDD<String, Long> userAvgTime = pairRDD.groupByKey().mapToPair(new PairFunction<Tuple2<String, Iterable<Long>>, String, Long>() {
+            public Tuple2<String, Long> call(Tuple2<String, Iterable<Long>> stringIterableTuple2) throws Exception {
+                Iterable<Long> longs = stringIterableTuple2._2;
+                Iterator<Long> iterator = longs.iterator();
+                long count = 0L;
+                long sum = 0L;
+                long avg = 0L;
+                long temp = 0L;
+                while (iterator.hasNext()) {
+                    if (count == 0) {
+                        temp = iterator.next();
+                    } else {
+                        long next = iterator.next();
+                        sum += (next - temp);
+                        temp = next;
+                    }
+                    count++;
+                }
+                avg = sum / count;
+                return new Tuple2<String, Long>(stringIterableTuple2._1, avg);
+            }
+        });
+
+    }
+}
--- spark-app/src/main/java/com/kong/spark/rdd/TopN.java
+++ spark-app/src/main/java/com/kong/spark/rdd/TopN.java
@@ -16,20 +16,17 @@ public class TopN {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("top5Java").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaPairRDD<Integer, Integer> pairRDD = sc.textFile("E:\\testData\\top5.txt").mapToPair(new PairFunction<String, Integer, Integer>() {
-            @Override
+        JavaPairRDD<Integer, Integer> pairRDD = sc.textFile("c:\\kong\\testData\\top5.txt").mapToPair(new PairFunction<String, Integer, Integer>() {
             public Tuple2<Integer, Integer> call(String s) throws Exception {
                 return new Tuple2<Integer, Integer>(Integer.parseInt(s), Integer.parseInt(s));
             }
         });
 
         pairRDD.sortByKey(false).map(new Function<Tuple2<Integer,Integer>, Integer>() {
-            @Override
-            public Integer call(Tuple2<Integer, Integer> result) throws Exception {
-                return result._2();
+            public Integer call(Tuple2<Integer, Integer> integerIntegerTuple2) throws Exception {
+                return integerIntegerTuple2._2;
             }
         }).take(5).forEach(new Consumer<Integer>() {
-            @Override
             public void accept(Integer integer) {
                 System.out.println(integer);
             }
