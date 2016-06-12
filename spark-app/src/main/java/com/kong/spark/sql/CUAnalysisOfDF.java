10fbad7d2380d3827b0a4f238e8c5e8cc7c2e46c
kong <konvish@163.com>
2016-06-12 15:46:46 +0800

    信令数据使用sql分析

--- spark-app/src/main/java/com/kong/spark/sql/CUAnalysisOfDF.java
+++ spark-app/src/main/java/com/kong/spark/sql/CUAnalysisOfDF.java
@@ -1,7 +1,6 @@
 package com.kong.spark.sql;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
@@ -9,6 +8,7 @@ import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.hive.HiveContext;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
@@ -17,35 +17,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by kong on 2016/3/16 0016.
+ * 使用sql进行多维度分析
+ * Created by lenovo on 2016/6/12.
  */
-public class RDD2DataFrameByProgrammatically {
+public class CUAnalysisOfDF {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("RDD2DataFrame2").setMaster("local");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        SQLContext sql = new SQLContext(sc);
-        JavaRDD<String> lines = sc.textFile("D://person.txt");
+        SparkConf conf = new SparkConf().setAppName("CUAnalysisOfDF").setMaster("local");
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+        SQLContext hiveContext = new HiveContext(jsc);
+        JavaRDD<String> javaRDD = jsc.textFile(args[0]);
+
         //在RDD的基础上创建类型为Row的RDD
-        JavaRDD<Row> personsRDD = lines.map(new Function<String, Row>() {
+        JavaRDD<Row> cuRDD = javaRDD.map(new Function<String, Row>() {
             public Row call(String line) throws Exception {
                 String[] splited = line.split(",");
-                return RowFactory.create(Integer.valueOf(splited[0]), splited[1], Integer.valueOf(splited[2]));
+                return RowFactory.create(Long.valueOf(splited[0]), Long.valueOf(splited[1]),Long.valueOf(splited[2]),Long.valueOf(splited[3]),Double.valueOf(splited[4]),Double.valueOf(splited[5]),Integer.valueOf(splited[6]));
             }
         });
 
         //动态构造DataFrame的元数据，一般而言，有多少列以及每列的具体类型可能来自于JSON文件,也可以来自数据库
         List<StructField> structFields = new ArrayList<StructField>();
-        structFields.add(DataTypes.createStructField("id",DataTypes.IntegerType,true));
-        structFields.add(DataTypes.createStructField("name",DataTypes.StringType,true));
-        structFields.add(DataTypes.createStructField("age",DataTypes.IntegerType,true));
+        structFields.add(DataTypes.createStructField("MSID",DataTypes.LongType,true));
+        structFields.add(DataTypes.createStructField("START_DATE",DataTypes.LongType,true));
+        structFields.add(DataTypes.createStructField("START_TIME",DataTypes.LongType,true));
+        structFields.add(DataTypes.createStructField("START_LONGITUDE",DataTypes.DoubleType,true));
+        structFields.add(DataTypes.createStructField("START_LATITUDE",DataTypes.DoubleType,true));
+        structFields.add(DataTypes.createStructField("areacode",DataTypes.IntegerType,true));
+
         //构建StructType，用于最后DataFrame元数据的描述
         StructType structType = DataTypes.createStructType(structFields);
         //基于以后的MetaData以及RDD<Row>来构造DataFrame
-        DataFrame dataFrame = sql.createDataFrame(personsRDD, structType);
+        DataFrame dataFrame = hiveContext.createDataFrame(cuRDD, structType);
         //注册成为临时表以供后续的SQL查询操作
-        dataFrame.registerTempTable("person");
+        dataFrame.registerTempTable("culocation");
         //进行数据的多维度分析
-        DataFrame result = sql.sql("select * from person where age < 10");
+        DataFrame result = hiveContext.sql("select * from culocation where MSID = 10001");
         //结果提取，转换成RDD，以及结构持久化
         List<Row> collect = result.javaRDD().collect();
         for (Row row:collect){
