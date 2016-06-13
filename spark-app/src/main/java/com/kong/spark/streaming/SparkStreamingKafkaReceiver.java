14dc2d7cb581f0efc28950846e91e587ae857340
kong <konvish@163.com>
2016-06-13 16:41:46 +0800

    ........

--- spark-app/src/main/java/com/kong/spark/streaming/SparkStreamingKafkaReceiver.java
+++ spark-app/src/main/java/com/kong/spark/streaming/SparkStreamingKafkaReceiver.java
@@ -23,25 +23,22 @@ public class SparkStreamingKafkaReceiver {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("kafkaReceiver").setMaster("spark://master:7077");
         JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(5));
-        Map<String,Integer> topic = new HashMap<>();
+        Map<String,Integer> topic = new HashMap<String, Integer>();
         topic.put("HelloKafka",2);//线程数
         JavaPairReceiverInputDStream<String, String> lines = KafkaUtils.createStream(jsc, "master:2181,worker1:2181,worker2:2181", "myConsumerGroup", topic);
         JavaDStream<String> flatMap = lines.flatMap(new FlatMapFunction<Tuple2<String, String>, String>() {
-            @Override
             public Iterable<String> call(Tuple2<String, String> tuple) throws Exception {
                 return Arrays.asList(tuple._2().split(" "));
             }
         });
 
         JavaPairDStream<String, Integer> pairDStream = flatMap.mapToPair(new PairFunction<String, String, Integer>() {
-            @Override
             public Tuple2<String, Integer> call(String s) throws Exception {
                 return new Tuple2<String, Integer>(s, 1);
             }
         });
 
         JavaPairDStream<String, Integer> reduceByKey = pairDStream.reduceByKey(new Function2<Integer, Integer, Integer>() {
-            @Override
             public Integer call(Integer integer, Integer integer2) throws Exception {
                 return integer + integer2;
             }
